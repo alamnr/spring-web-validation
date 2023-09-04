1ba2ff573ee34794e2cac39d4cf0ca4382e66012
alamnr <alamnr@gmail.com>
2023-09-04 10:59:55 +0600

    Initial commit

---
+++ src/main/java/com/example/springwebvalidation/SpringWebValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springwebvalidation/SpringWebValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
